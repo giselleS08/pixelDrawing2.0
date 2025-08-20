b442580ab5dabe9fe02d72a107c5746b94c40e1d
giselleS08 <giselle.sarabia8@gmail.com>
2025-08-20 20:46:05 +0000

    6 more pixels

--- script.js
+++ script.js
@@ -42,32 +42,40 @@ function setup() {
 
   //1st row
   pixel1 = new Sprite(10,10,19,19);
-  pixel1.rotation =0;
-  pixel1.collider = "kinematic";
-  pixel1.color = "black";
+  creatingPixels(pixel1);
 
   pixel2 = new Sprite(10,10,19,19);
-  pixel2.rotation =0;
-  pixel2.collider = "kinematic";
-  pixel2.color = "black";
+  creatingPixels(pixel2);
 
   pixel3 = new Sprite(10,10,19,19);
-  pixel3.rotation =0;
-  pixel3.collider = "kinematic";
-  pixel3.color = "black";
+  creatingPixels(pixel3);
 
   pixel4 = new Sprite(10,10,19,19);
-  pixel4.rotation =0;
-  pixel4.collider = "kinematic";
-  pixel4.color = "black";
+  creatingPixels(pixel4);
 
   pixel5 = new Sprite(10,10,19,19);
-  pixel5.rotation =0;
-  pixel5.collider = "kinematic";
-  pixel5.color = "black";
+  creatingPixels(pixel5);
 
   pixel6 = new Sprite(10,10,19,19);
   creatingPixels(pixel6);
+
+  pixel7 = new Sprite(10,10,19,19);
+  creatingPixels(pixel7);
+
+  pixel8 = new Sprite(10,10,19,19);
+  creatingPixels(pixel8);
+
+  pixel9 = new Sprite(10,10,19,19);
+  creatingPixels(pixel9);
+
+  pixel10 = new Sprite(10,10,19,19);
+  creatingPixels(pixel10);
+
+  pixel11 = new Sprite(10,10,19,19);
+  creatingPixels(pixel11);
+
+  pixel12 = new Sprite(10,10,19,19);
+  creatingPixels(pixel12);
 }
 
 
@@ -78,6 +86,12 @@ pixel3.pos={x:120+(2*19), y: 121};
 pixel4.pos={x:120+(3*19), y: 121};
 pixel5.pos={x:120+(4*19), y: 121};
 pixel6.pos={x:120+(5*19), y: 121};
+pixel7.pos={x:120+(6*19), y: 121};
+pixel8.pos={x:120+(7*19), y: 121};
+pixel9.pos={x:120+(8*19), y: 121};
+pixel10.pos={x:120+(9*19), y: 121};
+pixel11.pos={x:120+(10*19), y: 121};
+pixel12.pos={x:120+(11*19), y: 121};
 }
 
 function creatingPixels(sprite)
