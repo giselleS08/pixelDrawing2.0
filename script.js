2840ba24accf0d4e98fb897c344347e0ea8a4b71
giselleS08 <giselle.sarabia8@gmail.com>
2025-08-20 19:47:42 +0000

    first start

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
   background(255);
+
+
+  let findingPixelText = `
+  ..bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+
+  `;
+
+  let palette1 = {
+    b: color(175,0,0),//outline more than likely orange
+
+    s:color(255,25,25),//this will be the lighter orange
+    d:color(255,191,0),//yellow main color
+    f:color(255,93,0)//word color like black for visibility
+  };
+        startButton = new Sprite();
+        startButton.img = spriteArt(findingPixelText, 19, palette1);
+       startButton.pos={x:375, y: 300};
+        startButton.rotation =0;
+        startButton.collider = "kinematic";
 }
 
+
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+
 }
